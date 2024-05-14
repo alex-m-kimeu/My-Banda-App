1c0a0f8e6219b3989dc09c92563640fc3de0ac3d
Alex Kimeu <alexkimeu1999@gmail.com>
2024-05-15 01:31:47 +0300

    Added necessary project files

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'md': '0 0 10px rgba(0, 0, 0, 0.1)',
+      },
+      colors: {
+        'Primary': '#ffffff',
+        'Secondary': '#F3C82E',
+        'Variant': '#000000',
+        'Variant2': '#4B5563',
+        'Green': '#1F9254',
+        'Red': '#FF3C5F',
+        'Text': '#2d2e2e',
+      },
+      fontFamily:{
+        'body':"Open Sans"
+      }
+    },
   },
   plugins: [],
 }
