a1d8da58e25bdf939f9cac139e0629eb84e95549
Candy Bosibori Ongaro <candybosibori04@gmail.com>
2024-05-24 22:07:46 +0300

    update contact

--- src/Pages/Buyer/MyAccount/MyAccount.jsx
+++ src/Pages/Buyer/MyAccount/MyAccount.jsx
@@ -94,6 +94,35 @@ export const MyAccount = () => {
             console.error("Error:", error);
           });
       };
+
+      const handleSubmitContact = (e) => {
+        e.preventDefault();
+        const token = localStorage.getItem("token");
+        const decodedToken = jwtDecode(token);
+        const userId = decodedToken.sub.id;
+    
+        const userData = new FormData();
+        userData.append("contact", contact.contact);
+    
+        fetch(`http://127.0.0.1:5500/user/${userId}`, {
+          method: "PATCH",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+    
+          body: userData,
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Success:", data);
+            setUser(data);
+            setShowContactModal(false);
+            setContact({ contact: "" });
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+      };
   
   return (
     <div>
