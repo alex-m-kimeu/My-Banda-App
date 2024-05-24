39f192d56efda8d4370a5deca69508f2059939a2
Candy Bosibori Ongaro <candybosibori04@gmail.com>
2024-05-24 22:04:54 +0300

    State

--- src/Pages/Buyer/MyAccount/MyAccount.jsx
+++ src/Pages/Buyer/MyAccount/MyAccount.jsx
@@ -5,6 +5,20 @@ import toast from "react-hot-toast";
 
 
 export const MyAccount = () => {
+    const [user, setUser] = useState({});
+    const [showNameModal, setShowNameModal] = useState(false);
+    const [userNameData, setUsernameData] = useState({ username: "" });
+    const [showContactModal, setShowContactModal] = useState(false);
+    const [contact, setContact] = useState({ contact: "" });
+    const [showEmailModal, setShowEmailModal] = useState(false);
+    const [email, setEmail] = useState({ email: "" });
+    const [showImageModal, setShowImageModal] = useState(false);
+    const [image, setImage] = useState({ image: "" });
+    const [showPasswordModal, setShowPasswordModal] = useState(false);
+    const [password, setPassword] = useState({ current: "", newpassword: "", confirmpassword: "" });
+    const [preview, setPreview] = useState(null);
+    const imageInputRef = useRef();
+  
   return (
     <div>
       
