5f517e5f583c47d6c92be3b35b11b9f41a9fe708
Candy Bosibori Ongaro <candybosibori04@gmail.com>
2024-05-24 22:01:49 +0300

    added deatils code

--- src/Pages/Buyer/MyAccount/Details.jsx
+++ src/Pages/Buyer/MyAccount/Details.jsx
@@ -3,9 +3,49 @@ import { CiEdit } from "react-icons/ci";
 
 export const Details = () => {
   return (
-    <div>
+    <div className="px-5 ">
+    <div className=" mb-5">Edit Your Profile</div>
+
+    <div className="flex space-x-5">
+
+      <label htmlFor="" className="font-light  w-1/2 "><span className="text-[11px] font-medium">Username</span>
+      <div className="bg-gray-100 with mt-2 full flex justify-between px-2 py-1 rounded ">
+        <div className="text-Variant2 text-sm md:text-base">{user.username}</div> <CiEdit  onClick={() => { setShowNameModal(true)}} className=" w-4 h-4"/>
+      </div>
+      </label>
+      <label htmlFor="" className="font-light  w-1/2"><span className="text-[11px] font-medium">Image</span>
+      <div className="bg-gray-100 with mt-2 full flex justify-between px-2 py-1 rounded ">
+      <div className="text-Variant2 text-sm md:text-base">Upload a new image</div> <CiEdit  onClick={() => {setShowImageModal(true)}} className=" w-4 h-4"/>
+      </div>
+      </label>
+
+    </div>
+         <div className="flex space-x-5 mt-3">
+
+      <label htmlFor="" className="font-light  w-1/2 "><span className="text-[11px] font-medium">Email</span>
+      <div className="bg-gray-100 with mt-2 full flex justify-between px-2 py-1 rounded ">
+        <div className="text-Variant2 text-sm md:text-base">{user.email}</div> <CiEdit onClick={() => { setShowEmailModal(true)}} className=" w-4 h-4"/>
+      </div>
+      </label>
+      <label htmlFor="" className="font-light  w-1/2"><span className="text-[11px] font-medium">Contact</span>
+      <div className="bg-gray-100 with mt-2 full flex justify-between px-2 py-1 rounded ">
+      <div className="text-Variant2 text-sm md:text-base">{user.contact}</div> <CiEdit onClick={() => {setShowContactModal(true)}} className=" w-4 h-4"/>
+      </div>
+      </label>
+     
+    </div>
+
+    <div className="mt-2"> 
+    <label htmlFor="" className="font-light w-1/2"><span className="text-[11px] font-medium">Password</span>
+      <div className="bg-gray-100 with mt-2 full flex justify-between px-2 py-1 rounded ">
+      <div className="text-Variant2 text-sm md:text-base">Change your password</div> <CiEdit onClick={() => {setShowPasswordModal(true)}}className=" w-4 h-4"/>
+      </div>
+      </label>
       
     </div>
+    
+    
+  </div>
   )
 }
 
