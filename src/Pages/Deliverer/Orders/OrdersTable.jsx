dfdccc91e08301a7ec7131ad0e907cd8a2b67044
Hamdi Adan <hhhaji013@gmail.com>
2024-05-25 13:39:08 +0300

    OrdersTable

---
+++ src/Pages/Deliverer/Orders/OrdersTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const OrdersTable = ({ orders, onAcceptOrder, onDeclineOrder }) => {
+    return (
+        <div className="overflow-x-auto">
+
+        </div>
+    );
+};
