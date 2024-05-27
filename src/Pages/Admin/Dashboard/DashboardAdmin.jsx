7355366c47006bcb661b63c61f4a78f955392cd6
Peter00010 <128778434+Peter00010@users.noreply.github.com>
2024-05-27 21:41:17 +0300

    Feature/admin dash enhancement (#85)
    * Dashboard enhancement
    
    * Dashboard enhancement(padding and margin)
    
    * Dashboard enhancement(bar and line charts)
    
    * Dashboard enhancement(original dash styled)
    
    ---------
    
    Co-authored-by: Candy-O-Bosibori <157385135+Candy-O-Bosibori@users.noreply.github.com>

--- src/Pages/Admin/Dashboard/DashboardAdmin.jsx
+++ src/Pages/Admin/Dashboard/DashboardAdmin.jsx
@@ -1,3 +1,9 @@
+
+import { useEffect, useState } from "react";
+import { LiaShoppingCartSolid, LiaUsersSolid, LiaFileAltSolid, LiaHandHoldingUsdSolid } from "react-icons/lia";
+import { Pie } from 'react-chartjs-2';
+import { Chart, registerables } from 'chart.js';
+
 /* eslint-disable no-unused-vars */
 import { useState } from "react";
 import {
@@ -12,6 +18,7 @@ import {
 import bg from "../../../assets/admin.png";
 import { Pie, Doughnut } from "react-chartjs-2";
 import { Chart, registerables } from "chart.js";
+
 
 Chart.register(...registerables);
 
@@ -90,59 +97,53 @@ export const DashboardAdmin = () => {
     ],
   };
 
-  return (
-    <div className="p-4 space-y-4 text-xs sm:text-sm md:text-base">
-      <h1 className="text-Text font-bold text-xl text-center lg:text-left">
-        Dashboard
-      </h1>
-      <div className="flex flex-col lg:flex-row justify-between lg:justify-normal gap-4 lg:gap-16">
-        <div className="flex flex-col gap-4 md:gap-8">
-          <div className="flex items-center justify-between p-4 shadow-md rounded-md">
-            <div className="flex flex-col gap-4">
-              <h2 className="text-center text-Text font-semibold">
-                Welcome to My Banda
-              </h2>
-              <h3 className="text-center text-Secondary font-normal">Admin</h3>
-            </div>
-            <LiaStoreAltSolid className="fill-Secondary w-16 md:w-24 h-16 md:h-24 hidden sm:block" />
-          </div>
-          <img
-            src={bg}
-            alt="image"
-            className="w-full max-w-md mx-auto hidden sm:block"
-          />
-        </div>
-        <div className="flex flex-col gap-4 w-full">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div className="p-2 md:p-4 shadow-md rounded-md flex gap-2 md:gap-4 justify-center items-center w-full">
-              <LiaUsersSolid className="w-8 md:w-16 h-8 md:h-16 fill-Text" />
-              <div>
-                <h2 className="text-center text-Text font-semibold">Users</h2>
-                <p className="text-center">{nonAdminUsers.length}</p>
-              </div>
-            </div>
-            <div className="p-2 md:p-4 shadow-md rounded-md flex gap-2 md:gap-4 justify-center items-center w-full">
-              <LiaFileAltSolid className="w-8 md:w-16 h-8 md:h-16 fill-Text" />
-              <div>
-                <h2 className="text-center text-Text font-semibold">
-                  Complaints
-                </h2>
-                <p className="text-center">{complaints.length}</p>
-              </div>
-            </div>
-            <div className="p-2 md:p-4 shadow-md rounded-md flex gap-2 md:gap-4 justify-center items-center w-full">
-              <LiaShoppingCartSolid className="w-8 md:w-16 h-8 md:h-16 fill-Text" />
-              <div>
-                <h2 className="text-center text-Text font-semibold">Buyers</h2>
-                <p className="text-center">{buyers.length}</p>
-              </div>
-            </div>
-            <div className="p-2 md:p-4 shadow-md rounded-md flex gap-2 md:gap-4 justify-center items-center w-full">
-              <LiaHandHoldingUsdSolid className="w-8 md:w-16 h-8 md:h-16 fill-Text" />
-              <div>
-                <h2 className="text-center text-Text font-semibold">Sellers</h2>
-                <p className="text-center">{sellers.length}</p>
-              </div>
+    return (
+        <div className="p-4 space-y-4">
+            <h1 className="text-Text font-bold text-xl text-center lg:text-left">Dashboard</h1>
+            <div className="flex flex-col lg:flex-row lg:items-start lg:justify-between gap-6">
+                <div className="flex flex-col md:grid md:grid-cols-2 md:gap-4 lg:grid-cols-4 lg:gap-4 space-y-4 md:space-y-0">
+                    <div className="p-4 shadow-md rounded-md flex gap-4 justify-center items-center bg-Primary">
+                        <LiaUsersSolid className="w-10 h-10 fill-Text" />
+                        <div>
+                            <h2 className="text-center text-Text font-semibold text-sm md:text-base">Users</h2>
+                            <p className="text-center">{nonAdminUsers.length}</p>
+                        </div>
+                    </div>
+                    <div className="p-4 shadow-md rounded-md flex gap-4 justify-center items-center bg-Primary">
+                        <LiaFileAltSolid className="w-10 h-10 fill-Text" />
+                        <div>
+                            <h2 className="text-center text-Text font-semibold text-sm md:text-base">Complaints</h2>
+                            <p className="text-center">{complaints.length}</p>
+                        </div>
+                    </div>
+                    <div className="p-4 shadow-md rounded-md flex gap-4 justify-center items-center bg-Primary">
+                        <LiaShoppingCartSolid className="w-10 h-10 fill-Text" />
+                        <div>
+                            <h2 className="text-center text-Text font-semibold text-sm md:text-base">Buyers</h2>
+                            <p className="text-center">{buyers.length}</p>
+                        </div>
+                    </div>
+                    <div className="p-4 shadow-md rounded-md flex gap-4 justify-center items-center bg-Primary">
+                        <LiaHandHoldingUsdSolid className="w-10 h-10 fill-Text" />
+                        <div>
+                            <h2 className="text-center text-Text font-semibold text-sm md:text-base">Sellers</h2>
+                            <p className="text-center">{sellers.length}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center gap-6 w-full lg:w-1/2 mx-auto">
+                    <div className="w-full">
+                        <div className="h-64">
+                            <Pie data={userChartData} options={{ responsive: true, maintainAspectRatio: false }} />
+                        </div>
+                    </div>
+                    <div className="w-full">
+                        <div className="h-64">
+                            <Pie data={complaintChartData} options={{ responsive: true, maintainAspectRatio: false }} />
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <div className="p-2 md:p-4 shadow-md rounded-md flex gap-2 md:gap-4 justify-center items-center w-full">
               <LiaMoneyBillSolid className="w-8 md:w-16 h-8 md:h-16 fill-Text" />
@@ -193,9 +194,6 @@ export const DashboardAdmin = () => {
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  );
-};
-
+    );
+}
 
